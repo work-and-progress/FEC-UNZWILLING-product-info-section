3e0d8842bdda5d88109f5e109a24e4e32a3fbf8f
Ryan Shin <ryanminjaeshin@gmail.com>
2020-09-24 11:22:25 -0700

    setup webpack and react

---
+++ client/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+var App = () => {
+  return (
+    <div>
+      <h1>HEYY</h1>
+    </div>
+  )
+}
+ReactDOM.render(<App />, document.getElementById('app'));
