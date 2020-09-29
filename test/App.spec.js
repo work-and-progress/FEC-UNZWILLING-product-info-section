ae5e153437c2afb6a6f564e292cc86bf930551e6
Ryan Shin <ryanminjaeshin@gmail.com>
2020-09-28 22:22:36 -0700

    Use eznyme testing

--- test/App.spec.js
+++ test/App.spec.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import { configure, mount } from 'enzyme';
 import renderer from 'react-test-renderer';
-import { mount } from 'enzyme';
 
 import App, { Counter } from '../client/src/App';
+
+configure({ adapter: new Adapter() });
 
 describe('App', () => {
   test('snapshot renders', () => {
     const component = renderer.create(<App />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  // enzyme testing
+  it('renders the inner Counter', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find(Counter).length).toEqual(1);
   });
 });
 
