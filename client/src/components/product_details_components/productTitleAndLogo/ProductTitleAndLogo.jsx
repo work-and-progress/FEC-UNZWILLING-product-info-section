36bbf16f699f646e22044d3692f677325af0ac88
Ryan Shin <ryanminjaeshin@gmail.com>
2020-09-30 12:10:50 -0700

    Update ProductTitleAndLogo component

---
+++ client/src/components/product_details_components/productTitleAndLogo/ProductTitleAndLogo.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class ProductTitleAndLogo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hey</h1>
+      </div>
+    );
+  }
+}
+
+ProductTitleAndLogo.propTypes = {
+};
+
+ProductTitleAndLogo.defaultProps = {
+};
