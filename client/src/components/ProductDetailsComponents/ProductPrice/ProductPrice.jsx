d369d9dcfc1a655a95813187622ccb5e8520f482
Ryan Shin <ryanminjaeshin@gmail.com>
2020-10-01 21:23:19 -0700

    Basic rendering for ProductPrice component

--- client/src/components/ProductDetailsComponents/ProductPrice/ProductPrice.jsx
+++ client/src/components/ProductDetailsComponents/ProductPrice/ProductPrice.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+// Product prices should be refactored.
 const propTypes = {
   productInfoDetails: PropTypes.instanceOf(Object),
 };
@@ -12,14 +12,29 @@ const defaultProps = {
 export default function ProductPrice(props) {
   const { productInfoDetails } = props;
 
+  const discountPrice = (price, discount) => (
+    price * ((100 - discount) / 100)
+  );
+
+  const discountedPrice = discountPrice(productInfoDetails.price, productInfoDetails.discount);
+
   return (
     <div>
       <div>
         <p>
+          IN STOCK | SHIPS WITHIN 1-2 DAYS
+        </p>
+        <p>
           {`YOU SAVE: - ${productInfoDetails.discount}%`}
         </p>
         <p>
-          {productInfoDetails.price}
+          {`$${productInfoDetails.price}`}
+        </p>
+        <p>
+          {`$${discountedPrice}`}
+        </p>
+        <p>
+          WRITE A REVIEW
         </p>
       </div>
     </div>
