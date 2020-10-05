13e569eefe220896ffcef16bc77b6313d2ba0f4b
Ryan Shin <ryanminjaeshin@gmail.com>
2020-10-05 01:49:44 -0700

    Update seeding and update ProductReviewDropdown component

--- client/src/components/ProductDetailsComponents/ProductReviewDropdown/ProductReviewDropdown.jsx
+++ client/src/components/ProductDetailsComponents/ProductReviewDropdown/ProductReviewDropdown.jsx
@@ -1,10 +1,77 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './styles.css';
 
 export default function ProductReviewDropDown(props) {
+  const {
+    productInfoDetails: {
+      review: {
+        numberOfReviews,
+        fivStarReviews,
+        fourStarReviews,
+        threeStarReviews,
+        twoStarReviews,
+        oneStarReviews,
+        averageRating,
+      } = {},
+    },
+  } = props;
+
+  const reviewRatingSummary = [
+    fivStarReviews,
+    fourStarReviews,
+    twoStarReviews,
+    twoStarReviews,
+    oneStarReviews,
+  ];
+
+  console.log(reviewRatingSummary);
+  console.log(numberOfReviews);
+
+  const averageRatingPercentage = (rating) => {
+    const percentageRating = (rating / 5) * 100;
+    return percentageRating;
+  };
+
+  const changeReviewSummaryToPercentage = reviewRatingSummary.map((item) => (
+    averageRatingPercentage(item)
+  ));
+
+  console.log(changeReviewSummaryToPercentage);
+
+  const averageRatingPercentageValue = averageRatingPercentage(averageRating);
+
   return (
     <div>
-      {console.log('HI I AM DROPDOWN')}
+      <div className={styles.ratingContainer}>
+        <div className={styles.ratings}>
+          <div className={styles.emptyStars} />
+          <div
+            className={styles.fullStars}
+            style={{ width: `${averageRatingPercentageValue}%` }}
+          />
+        </div>
+      </div>
+      <div className={styles.ratingContainer}>
+        <div className={styles.ratings}>
+          <div className={styles.emptyStars} />
+          <div
+            className={styles.fullStars}
+            style={{ width: `${averageRatingPercentageValue}%` }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
+
+const propTypes = {
+  productInfoDetails: PropTypes.instanceOf(Object),
+};
+
+const defaultProps = {
+  productInfoDetails: null,
+};
+
+ProductReviewDropDown.propTypes = propTypes;
+ProductReviewDropDown.defaultProps = defaultProps;
--- client/src/components/ProductDetailsComponents/ProductReviews/ProductReviews.jsx
+++ client/src/components/ProductDetailsComponents/ProductReviews/ProductReviews.jsx
@@ -4,13 +4,22 @@ import PropTypes from 'prop-types';
 import styles from './styles.css';
 import starImage from './star.png';
 import emptyStarImage from './empty_star.png';
+import ProductReviewDropdown from '../ProductReviewDropdown/ProductReviewDropdown';
 
 export default class ProductReviews extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      displayDropdown: false,
     };
+    this.displayProductReviewDropdown = this.displayProductReviewDropdown.bind(this);
+  }
+
+  displayProductReviewDropdown() {
+    const { displayDropdown } = this.state;
+    this.setState({
+      displayDropdown: !(displayDropdown),
+    });
   }
 
   render() {
@@ -26,7 +35,10 @@ export default class ProductReviews extends React.Component {
           averageRating,
         } = {},
       },
+      productInfoDetails,
     } = this.props;
+
+    const { displayDropdown } = this.state;
 
     const averageRatingPercentage = (rating) => {
       const percentageRating = (rating / 5) * 100;
@@ -35,8 +47,18 @@ export default class ProductReviews extends React.Component {
 
     const averageRatingPercentageValue = averageRatingPercentage(averageRating);
 
+    let productReviewDropdown = null;
+
+    if (displayDropdown) {
+      productReviewDropdown = (<ProductReviewDropdown productInfoDetails={productInfoDetails} />);
+    }
+
     return (
-      <div className={styles.container}>
+      <div
+        className={styles.container}
+        onMouseEnter={this.displayProductReviewDropdown}
+        onMouseLeave={this.displayProductReviewDropdown}
+      >
         <div className={styles.ratings}>
           <div className={styles.emptyStars} />
           <div
@@ -51,6 +73,7 @@ export default class ProductReviews extends React.Component {
           {numberOfReviews}
           )
         </div>
+        {productReviewDropdown}
       </div>
     );
   }
